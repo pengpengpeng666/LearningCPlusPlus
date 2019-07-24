c7d94884630df9a5823efbf53d0cf34ec0b315fa
pengpeng <scutlxp@163.com>
2019-07-24 10:10:09 +0800

    vector clear

---
+++ 3.Container/3.1Vector/clear.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+static void printIntVector(vector<int> &nums)
+{
+    int i;
+
+    if (nums.size() ==0) {
+        cout<<"INFO: empty vector"<<endl;
+        return;
+    }
+
+    for (i=0; i<nums.size()-1; i++) {
+        cout<<nums[i]<<", ";
+    }
+    cout<<nums[i]<<endl;
+}
+
+
+int main(int argn, char **argv)
+{
+    vector<int> nums;
+
+    for (int i=0; i<10; i++) {
+        nums.push_back(i);
+    }
+
+    printIntVector(nums);
+
+    cout<<endl;
+
+    nums.clear();
+
+    printIntVector(nums);
+
+    return 0;
+}
